99440381a599123c67214aa18bd3041e914714c9
engelberthtogutimothysiahaan <79085738+engelberthtogutimothysiahaan@users.noreply.github.com>
2026-05-28 20:35:46 +0700

    Update GoToLeaderboard.cs

--- GoToLeaderboard.cs
+++ GoToLeaderboard.cs
@@ -6,6 +6,6 @@ public class GoToLeaderboard : MonoBehaviour
     public void OnLeaderboardButton()
     {
         SceneManager.LoadScene("Leaderboard"); 
-        // Atau ganti dengan nama scene leaderboard kamu jika berbeda
+
     }
 }
