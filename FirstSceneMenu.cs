c74ed1465f119a2257b5974079ff995df7fefb66
engelberthtogutimothysiahaan <79085738+engelberthtogutimothysiahaan@users.noreply.github.com>
2026-05-28 19:36:35 +0700

    Update FirstSceneMenu.cs

--- FirstSceneMenu.cs
+++ FirstSceneMenu.cs
@@ -6,12 +6,7 @@ public class FirstSceneMenu : MonoBehaviour
     // Nama scene tutorial yang akan dimuat saat tombol Start diklik
     [SerializeField] private string tutorialSceneName = "TutorialScene"; // Ganti dengan nama scene 
 
-    // Fungsi ini akan dipanggil saat tombol Start diklik
-    public void StartTutorial()
-    {
-        Debug.Log("Loading tutorial scene...");
-        SceneManager.LoadSceneAsync(tutorialSceneName);
-    }
+  
 
     // Fungsi ini akan dipanggil saat tombol Exit diklik
     public void ExitGame()
